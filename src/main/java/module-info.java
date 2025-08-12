3a20cfdefbd26959d3b4282ca09dc15d48192b75
Pedroj-64 <pedroj.sotor@uqvirtual.edu.co>
2025-08-12 14:52:25 -0500

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module co.edu.uniquindio.poo.veterinaria {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens co.edu.uniquindio.poo.veterinaria to javafx.fxml;
+    exports co.edu.uniquindio.poo.veterinaria;
+}
